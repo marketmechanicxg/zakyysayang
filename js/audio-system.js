0a4861fa51b4e374bfb1dcf980f5e5deffe92c55
marketmechanicxg <grizzyxixi@gmail.com>
2026-05-24 16:32:22 +0700

    Update audio-system.js

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'Risk it All',
+    artist:    'Bruno Mars',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
